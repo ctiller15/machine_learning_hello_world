1ae66c712295923953c5776fd3b088943dd8e094
ctiller15 <christiller2015@gmail.com>
2018-06-19 21:31:46 -0400

    add comments for personal use

--- recipe.py
+++ recipe.py
@@ -1,9 +1,14 @@
 from sklearn import tree
+# First column = weight in grams
+# Second column = texture. 1 = smooth, 0 = bumpy.
 features = [[140, 1],
             [130, 1],
             [150, 0],
             [170, 0]]
+
+# 0 = apple, 1 = orange
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
+# should predict an orange!
 print(clf.predict([[150, 0]]))
